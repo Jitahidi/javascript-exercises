c1c3dc01d141976abd2d8023aa5ea2fb4cad32e9
Jitahidi <morelporter@gmail.com>
2022-12-03 10:27:54 -0500

    Finished repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let holdString = "";
+    if (number < 0) {
+        return 'ERROR'
+    }
 
+    if (string === '') {
+        return ''
+    }
+
+    for (let i = 0; i < number; i++) {
+        holdString = holdString + string;
+    }
+    return holdString
 };
 
 // Do not edit below this line
