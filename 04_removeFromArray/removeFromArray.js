7207e8843d3ffadfc45a4d4af18e32841eb39503
Jitahidi <morelporter@gmail.com>
2022-12-27 12:26:04 -0500

    Finished removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removeElement1, removeElement2, removeElement3, removeElement4) {
+    let myArray = array;
+    let removeValues = [removeElement1, removeElement2, removeElement3, removeElement4];
+    myArray = myArray.filter(item => !removeValues.includes(item));
+    console.log(myArray);
+    return myArray
 };
 
 // Do not edit below this line
