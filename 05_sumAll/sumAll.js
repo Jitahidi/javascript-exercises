1a766ce64a8352630e736b087429fe8f933cbcc1
Jitahidi <morelporter@gmail.com>
2022-12-27 14:25:48 -0500

    Finished sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+let numOfIntegers;
 
+if (lastNum !== Number(lastNum)) {
+    return 'ERROR'
+}
+
+if (lastNum > firstNum && firstNum > 0) {
+    numOfIntegers = (lastNum - firstNum) + 1;
+} else if (lastNum < firstNum && lastNum > 0) {
+    numOfIntegers = Math.abs(lastNum - firstNum) + 1;
+} else {
+    return 'ERROR'
+}
+    
+let sumOfConsecutiveIntegers = (numOfIntegers * (firstNum + lastNum)) / 2;
+console.log(sumOfConsecutiveIntegers);
+return sumOfConsecutiveIntegers
 };
 
 // Do not edit below this line
